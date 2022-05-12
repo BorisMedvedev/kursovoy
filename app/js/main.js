c1883d2ff32d59a19c065df7f57192643b065da1
Your Name <you@example.com>
2022-05-13 00:13:06 +0300

    add footer

--- app/js/main.js
+++ app/js/main.js
@@ -58,3 +58,61 @@ var swiper = new Swiper(".project-slider", {
 	// 	clickable: true,
 	// },
 });
+
+
+ymaps.ready(init);
+
+function init() {
+	var myMap = new ymaps.Map("contacts__map", {
+		center: [55.75846806898367, 37.60108849999989],
+		zoom: 16
+	});
+
+	var myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989], {}, {
+		iconLayout: 'default#image',
+		iconImageHref: '../images/icons/group.svg',
+		iconImageSize: [20, 20],
+		iconImageOffset: [-3, -42]
+	});
+	myMap.geoObjects.add(myPlacemark);
+	myMap.behaviors.disable("scrollZoom");
+}
+
+var total_pics_num = 3; // колличество изображений
+var interval = 5000; // задержка между изображениями
+var time_out = 25; // задержка смены изображений
+var i = 0;
+var timeout;
+var opacity = 100;
+
+function fade_to_next() {
+	opacity--;
+	var k = i + 1;
+	var image_now = 'image_' + i;
+	if (i == total_pics_num) k = 1;
+	var image_next = 'image_' + k;
+	document.getElementById(image_now).style.opacity = opacity / 100;
+	document.getElementById(image_now).style.filter = 'alpha(opacity=' + opacity + ')';
+	document.getElementById(image_next).style.opacity = (100 - opacity) / 100;
+	document.getElementById(image_next).style.filter = 'alpha(opacity=' + (100 - opacity) + ')';
+	timeout = setTimeout("fade_to_next()", time_out);
+	if (opacity == 1) {
+		opacity = 100;
+		clearTimeout(timeout);
+	}
+}
+setInterval(
+	function () {
+		i++;
+		if (i > total_pics_num) i = 1;
+		fade_to_next();
+	}, interval
+);
+
+
+tippy('.tultip-1', {
+	content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
+});
+tippy('.tultip-2', {
+	content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
+});
