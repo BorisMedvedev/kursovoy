8cd8897363513a807676b91abda55066190c237c
Your Name <you@example.com>
2022-05-14 19:50:23 +0300

    999

--- app/js/main.js
+++ app/js/main.js
@@ -1,91 +1,98 @@
 (function ($) {
-	$(function () {
-		$('.filter-styles').styler();
-	});
-
-	new Accordion('.accordion', {
-
-
-	});
-
-
+  $(function () {
+    $('.filter-styles').styler();
+  });
+  new Accordion('.accordion', {});
 })(jQuery);
 
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper1 = new Swiper(".gallery-slider__inner", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    slidesPerGroup: 1,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: ".slider-btn--next",
+      prevEl: ".slider-btn--prev",
+    },
+    pagination: {
+      el: ".gallery-slider__pagination",
+      type: "fraction",
+      clickable: true,
+    },
+  });
 
-var swiper = new Swiper(".gallery-slider__inner", {
-	slidesPerView: 3,
-	spaceBetween: 50,
-	slidesPerGroup: 1,
-	navigation: {
-		nextEl: ".slider-btn--next",
-		prevEl: ".slider-btn--prev",
-	},
-	pagination: {
-		el: ".gallery-slider__pagination",
-		type: "fraction",
-		clickable: true,
-	},
-});
+  var swiper2 = new Swiper(".events-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    slidesPerGroup: 1,
+    loop: true,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: ".events-swiper__next",
+      prevEl: ".slider-btn--prev",
+    },
+    pagination: {
+      el: ".gallery-slider__pagination",
+      type: "fraction",
+      clickable: true,
+    },
+  });
+  var swiper3 = new Swiper(".project-slider", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    loop: true,
+    navigation: {
+      nextEl: ".project-slider__btn--next",
+      prevEl: ".project-slider__btn--prev",
+    },
 
-var swiper = new Swiper(".events-swiper", {
-	slidesPerView: 3,
-	spaceBetween: 50,
-	slidesPerGroup: 1,
-	loop: true,
-	navigation: {
-		nextEl: ".events-swiper__next",
-		prevEl: ".slider-btn--prev",
-	},
-	pagination: {
-		el: ".gallery-slider__pagination",
-		type: "fraction",
-		clickable: true,
-	},
-});
-var swiper = new Swiper(".project-slider", {
-	slidesPerView: 3,
-	spaceBetween: 50,
-	slidesPerGroup: 1,
-	centeredSlides: true,
-	loop: true,
-	navigation: {
-		nextEl: ".project-slider__btn--next",
-		prevEl: ".project-slider__btn--prev",
-	},
-	// pagination: {
-	// 	el: ".gallery-slider__pagination",
-	// 	type: "fraction",
-	// 	clickable: true,
-	// },
-});
-
-
-ymaps.ready(init);
-
-function init() {
-	var myMap = new ymaps.Map("contacts__map", {
-		center: [55.75846806898367, 37.60108849999989],
-		zoom: 16
-	});
-
-	var myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989], {}, {
-		iconLayout: 'default#image',
-		iconImageHref: '../images/icons/group.svg',
-		iconImageSize: [20, 20],
-		iconImageOffset: [-3, -42]
-	});
-	myMap.geoObjects.add(myPlacemark);
-	myMap.behaviors.disable("scrollZoom");
-}
+  });
 
 
-tippy('.tultip-1', {
-	content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
-});
+  ymaps.ready(init);
 
-tippy('.tultip-2', {
-	content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
-});
-tippy('.tultip-3', {
-	content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
+  function init() {
+    var myMap = new ymaps.Map("contacts__map", {
+      center: [55.75846806898367, 37.60108849999989],
+      zoom: 16
+    });
+
+    var myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../images/icons/group.svg',
+      iconImageSize: [20, 20],
+      iconImageOffset: [-3, -42]
+    });
+    myMap.geoObjects.add(myPlacemark);
+    myMap.behaviors.disable("scrollZoom");
+  }
+
+
+  tippy('.tultip-1', {
+    content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
+  });
+
+  tippy('.tultip-2', {
+    content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
+  });
+  tippy('.tultip-3', {
+    content: 'Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно Предварительные выводы: постоянное информационно-пропагандистское обеспечение нашей деятельности однозначно',
+  });
+
+
+
+
 });
