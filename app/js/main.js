a9e607812b424622a70d3fad5acf27a741eb36c2
Your Name <you@example.com>
2022-05-22 23:42:38 +0300

    mask

--- app/js/main.js
+++ app/js/main.js
@@ -192,3 +192,27 @@ document.addEventListener("DOMContentLoaded", function () {
   tulTip();
   tabsContent();
 });
+
+var Selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7 (999)-999-99-99");
+im.mask(Selector);
+new JustValidate(".contacts-form", {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 10,
+    },
+    phone: {
+      required: true,
+      function: (name, value) => {
+        const phone = Selector.Inputmask.unmaskevalue();
+        return Number(phone) && phone.length === 10;
+      },
+    },
+    mail: {
+      required: true,
+      email: true,
+    },
+  },
+});
